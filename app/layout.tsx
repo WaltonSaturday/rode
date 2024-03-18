6c215bcd0f4750278be3e598120468bb9c4f41e1
MatEhm <info@modeltwin.com>
2024-03-18 18:25:03 +0100

    bg gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,12 @@ export default function RootLayout({
     >
       <body
         // className="dark:bg-gradient-to-r  dark:from-pink-300 dark:via-purple-300 dark:to-fuchsia-300 bg-size-400 animation-gradient opacity-90  text-gray-950 relative pt-28 sm:pt-36 dark:bg-[#040309] dark:text-gray-50 dark:text-opacity-90"
-        className="bg-pink-200 text-gray-950 relative pt-28 sm:pt-36 dark:bg-[#040309] dark:text-gray-50 dark:text-opacity-90"
+        className="bg-pink-100 text-gray-950 relative pt-28 sm:pt-36 dark:bg-[#040309] dark:text-gray-50 dark:text-opacity-90"
         // className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             {/* <StarsCanvas /> */}
